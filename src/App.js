379659c0c74701349a36ba7eec102131d5285c8c
mogy-h <rapter.mg@gmail.com>
2021-08-21 00:09:05 +0900

    xivapiを叩いて、名前でキャラ検索できる遊び

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  // const [inputName, setInputName] = useState("");
+  // const [inputServer, setInputServer] = useState("");
+  const [input, setInput] = useState("");
+  const [name, setName] = useState("");
+  // const [server, setServer] = useState("");
+  const [icon, setIcon] = useState("");
+
+  const xiv = axios.create({
+    baseURL: "https://xivapi.com",
+  });
+
+  const fetchCharacterData = async () => {
+    return await xiv.get("/character/search", {
+      params: {
+        name: name,
+        server: "Chocobo",
+      },
+    });
+  };
+
+  useEffect(() => {
+    fetchCharacterData().then((res) => {
+      console.log(res.data.Results[0]);
+      setName(res.data.Results[0].Name);
+      setIcon(res.data.Results[0].Avatar);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [name]);
+
+  const setValue = (e) => {
+    e.preventDefault();
+    setName(input);
+    setInput("")
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          value={input}
+          placeholder="名前を入力"
+        />
+        <button onClick={setValue}>送信</button>
+      </div>
+
+      <img src={icon} alt="キャラアイコン" />
+      <p> {name} </p>
     </div>
   );
 }
